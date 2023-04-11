850b7e60b7391cf512f530c25eb69108925660a8
João Marcos Piccinin <128412664+joaopiccinin@users.noreply.github.com>
2023-04-11 08:26:11 -0300

    Update analisededados.py

--- analisededados.py
+++ analisededados.py
@@ -2,18 +2,7 @@
 # coding: utf-8
 
 # # Análise de Dados com Python
-# 
-# ### Desafio:
-# 
-# Você trabalha em uma empresa do varejo e tem milhares de clientes diferentes.
-# 
-# Com o objetivo de aumentar o faturamento e o lucro da sua empresa, a diretoria quer conseguir identificar quem é o cliente ideal para seus produtos, baseado no histórico de compras dos clientes.
-# 
-# Para isso, ela fez um trabalho de classificar os clientes com uma nota de 1 a 100. Só que agora, sobrou para você conseguir, a partir dessa nota, descobrir qual o perfil de cliente ideal da empresa.
-# 
-# Qual a profissão? Qual a idade? Qual a faixa de renda? E todas as informações que você puder analisar para dizer qual o cliente ideal da empresa.
-# 
-# Base de Dados: https://drive.google.com/drive/folders/1T7D0BlWkNuy_MDpUHuBG44kT80EmRYIs?usp=share_link
+
 
 # In[13]:
 
@@ -62,25 +51,6 @@ for coluna in tabela.columns:
 #clientes com 10-15 anos de experiencia de trabalho são melhores
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
